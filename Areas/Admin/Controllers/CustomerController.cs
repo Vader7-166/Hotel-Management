c8e8f16efafb6172725008397257b36cf481eab6
UrSweetHeart <tranxuantuan1522005@gmail.com>
2025-10-08 01:30:46 +0700

    feat[Customer]: implement Customer Management's Actions
    just done briefly implementing CustomerList.cshtml, AddCustomer.cshtml, did not dive in details

--- Areas/Admin/Controllers/CustomerController.cs
+++ Areas/Admin/Controllers/CustomerController.cs
@@ -2,11 +2,30 @@
 
 namespace Hotel_Management.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CustomerController : Controller
     {
-        [Area("Admin")]
         [Route("Admin/Customer")]
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CustomerList()
+        {
+            return View();
+        }
+
+        public IActionResult AddNewCustomer()
+        {
+            return View();
+        }
+
+        public IActionResult CustomerSegmentation()
+        {
+            return View();
+        }
+
+        public IActionResult CustomerReport() 
         {
             return View();
         }
