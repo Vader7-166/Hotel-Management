a117e2ab58c43129c04f359dbda880517f600766
hathen <hamanhth2005@gmail.com>
2025-10-07 18:20:42 +0700

    create layout for customer

--- Areas/Customer/Controllers/AccountController.cs
+++ Areas/Customer/Controllers/AccountController.cs
@@ -2,11 +2,19 @@
 //đăng ký đăng nhập
 namespace Hotel_Management.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class AccountController : Controller
     {
+        [Route("Customer/Account/Index")]
         public IActionResult Index()
         {
             return View();
         }
+        [Route("Customer/Account/Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
     }
 }
--- Areas/Customer/Controllers/RoomsController.cs
+++ Areas/Customer/Controllers/RoomsController.cs
@@ -2,8 +2,10 @@
 
 namespace Hotel_Management.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class RoomsController : Controller
     {
+        [Route("Customer/Rooms/Index")]
         public IActionResult Index()
         {
             return View();
