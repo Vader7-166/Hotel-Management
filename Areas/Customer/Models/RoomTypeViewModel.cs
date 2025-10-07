b111e2a83138c333851d64ce8a0d36b31f639bf1
caohagit <caov77029@gmail.com>
2025-10-07 21:46:14 +0700

    addfolderImageroom

---
+++ Areas/Customer/Models/RoomTypeViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotel_Management.Areas.Customer.Models
+{
+    public class RoomTypeViewModel
+    {
+        public int Id { get; set; }
+        public string TypeName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
