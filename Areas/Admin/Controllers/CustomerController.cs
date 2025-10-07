78a657ef5dcc1fb6f2ab4e0c0a6dc192328dd4cd
UrSweetHeart <tranxuantuan1522005@gmail.com>
2025-10-07 21:05:45 +0700

    feat[Customer]: Implement initial structure for Customer Managenment landing page
    This commit is the starting point of the creating Customer Management feature, start creating _CustomerManagementLayout.cshtml, setup route , and redirecting route to the CustomerManagement Page

--- Areas/Admin/Controllers/CustomerController.cs
+++ Areas/Admin/Controllers/CustomerController.cs
@@ -4,6 +4,8 @@ namespace Hotel_Management.Areas.Admin.Controllers
 {
     public class CustomerController : Controller
     {
+        [Area("Admin")]
+        [Route("Admin/Customer")]
         public IActionResult Index()
         {
             return View();
