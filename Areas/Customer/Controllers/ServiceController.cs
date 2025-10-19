f0418d97e909517e2e81086cfd61ee79a1e93de5
caohagit <caov77029@gmail.com>
2025-10-19 20:11:41 +0700

    create[layout] for services page

---
+++ Areas/Customer/Controllers/ServiceController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel_Management.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("Customer/Service/MeetEvents")]
+        public IActionResult MeetEvents()
+        {
+            return View();
+        }
+        [Route("Customer/Service/Spa")]
+        public IActionResult Spa()
+        {
+            return View();
+        }
+        [Route("Customer/Service/SwimmingPool")]
+        public IActionResult SwimmingPool()
+        {
+            return View();
+        }
+    }
+}
