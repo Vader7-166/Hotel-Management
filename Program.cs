9e5a4ecea1fd906ae5ffae616286171263e3f351
duyhung753951 <duyhung160605@gmail.com>
2026-02-25 19:57:13 +0700

    fix

--- Program.cs
+++ Program.cs
@@ -70,7 +70,13 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
-app.Run($"http://0.0.0.0:{port}");
+var port = Environment.GetEnvironmentVariable("PORT");
 
-app.Run();
+if (!string.IsNullOrEmpty(port))
+{
+    app.Run($"http://0.0.0.0:{port}");
+}
+else
+{
+    app.Run();
+}
